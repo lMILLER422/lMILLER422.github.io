4a29f60fa30fc7064fbdefc2c30d08e8fdffae85
lMILLER422 miller <ljm4299992@lpssonline.com>
2023-09-14 23:42:58 +0000

    <Epic Game>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+     for (let i = 100; i < canvas.width; i += 100) {
+       createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+    for (let i = 100; i < canvas.height; i += 100) {
+       createPlatform(canvas.width, i, -canvas.width, -1);
+     }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -43,6 +43,31 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    // spawning platform
+    createPlatform(0,625,200,20)
+    createPlatform(0,625,20,200)
+    createPlatform(0,425,20,200)
+    //
+    createPlatform(200,0,20,225)
+    createPlatform(200,225,200,20)
+    //1st jump
+    createPlatform(400,450,200,20)
+    createPlatform(400,574,200,20)
+    createPlatform(600,450,20,300)
+    createPlatform(400,340,200,20)
+    createPlatform(600,340,20,144)
+    createPlatform(675,275,.1,.1)
+    createPlatform(900,250,200,20)
+    createPlatform(900,250,20,400)
+    createPlatform(900,50,20,200)
+    createPlatform(900,125,200,20)
+    //second platform
+    createPlatform(1000,480,200,20)
+    createPlatform(1000,604,200,20)
+    createPlatform(1200,380,20,300)
+    createPlatform(1000,360,200,20)
+    createPlatform(1200,360,20,144)
+  
 
 
 
@@ -52,8 +77,11 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("steve", 250, 150, 20, 0.5);
+    createCollectable("grace", 100, 700);
+    createCollectable("steve", 1000, 0, 20, 0.5);
+    createCollectable("grace", 1275, 700);
+    
 
 
     // TODO 3
@@ -61,7 +89,11 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("left", 600, 3000, );
+    createCannon("top", 800, 1250, );
+    createCannon("top", 1200, 3000, );
+    createCannon("top", 1300, 3000, );
+    createCannon("top", 1400, 4000, );
 
 
 
